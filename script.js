1b98e8529bb53a00312affc88b7e899e6a3a8dae
Mdajaj123 <137004218+Mdajaj123@users.noreply.github.com>
2023-12-16 20:41:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function firstChar(text) {
+function firstChar(str) {
   // your code here
-    if(text.charAt(0)==""){
-		return "";
-	}
-	return text.charAt(0)
+ //    if(text.charAt(0)==""){
+	// 	return "";
+	// }
+	// return text.charAt(0)
+	  // Remove leading and trailing spaces
+  const trimmedStr = str.trim();
+
+  // Check if the trimmed string is empty
+  if (trimmedStr === '') {
+    return '';
+  }
+
+  // Return the first character of the trimmed string
+  return trimmedStr.charAt(0);
+
 }
 
 // Do not change the code below
